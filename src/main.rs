8d628a630296e8730d6f1aaa57829495aec962b9
Josh Brunton <josh.brunton@proton.me>
2024-10-21 10:48:02 +0100

    Add code to find all git repos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::BufRead;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    match get_repos() {
+        Ok(lines) => {
+            for line in lines {
+                println!("{}", line);
+            }
+        }
+        Err(e) => eprintln!("Error: {}", e),
+    }
+}
+
+fn get_repos() -> Result<Vec<String>, String> {
+    let stdout = Command::new("locate")
+        .arg("*.git")
+        .spawn()
+        .expect("ls command failed to start")
+        .wait_with_output()
+        .expect("Could not capture output")
+        .stdout;
+    let lines: Result<Vec<String>, std::io::Error> = stdout.lines().collect();
+
+    // Return the lines or propagate any IO errors
+    lines.map_err(|e| format!("Failed to read lines: {}", e))
 }
