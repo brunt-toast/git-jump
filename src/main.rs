01bec812303be6ef28acd736fec7146b0fdd902a
Josh Brunton <josh.brunton@proton.me>
2024-10-25 12:52:46 +0100

    Add support for svn, hg, p4, bzr, cvs, tfvc

--- src/main.rs
+++ src/main.rs
@@ -23,26 +23,40 @@ fn main() {
 fn get_repos() -> Result<Vec<String>, String> {
     let conf = config::Config::from_json_file().expect("Could not load configuration");
 
-    let stdout = Command::new("locate")
-        .arg("*.git")
+    let ret_unfiltered = get_repos_of_type(".git")
+        .into_iter()
+        .chain(get_repos_of_type(".svn"))
+        .chain(get_repos_of_type(".hg"))
+        .chain(get_repos_of_type("p4root"))
+        .chain(get_repos_of_type(".p4config"))
+        .chain(get_repos_of_type(".bzr"))
+        .chain(get_repos_of_type("CVS"))
+        .chain(get_repos_of_type("CVSROOT"))
+        .chain(get_repos_of_type("$tf"))
+        .chain(get_repos_of_type(".tfs"))
+        .collect();
+
+    let ret_with_whitelist = conf.clone().filter_whitelist(ret_unfiltered);
+    let ret_with_blacklist = conf.filter_blacklist(ret_with_whitelist);
+    Ok(ret_with_blacklist)
+}
+
+fn get_repos_of_type(repo_type: &str) -> Vec<String> {
+    let git = Command::new("locate")
+        .arg("*".to_owned() + repo_type)
         .output()
         .expect("Failed to capture output")
         .stdout;
-    let lines: Result<Vec<String>, std::io::Error> = stdout.lines().collect();
-
-    // Return the lines or propagate any IO errors
-    let ret_unfiltered = lines
+    let stdout: Result<Vec<String>, std::io::Error> = git.lines().collect();
+    let mut lines = stdout
         .map_err(|e| format!("Failed to read lines: {}", e))
         .expect("Failed to read lines");
 
-    let ret_with_whitelist = conf.clone().filter_whitelist(ret_unfiltered);
-    let mut ret_with_blacklist = conf.filter_blacklist(ret_with_whitelist);
-
-    for item in ret_with_blacklist.iter_mut() {
-        if let Some(s) = item.strip_suffix(".git") {
+    for item in lines.iter_mut() {
+        if let Some(s) = item.strip_suffix(repo_type) {
             *item = s.to_string();
         }
     }
 
-    Ok(ret_with_blacklist)
+    return lines;
 }
